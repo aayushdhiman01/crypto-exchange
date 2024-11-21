69ebbcb29c6f2597f5fde24de5c94922434a983a
JATINDERSsINGH <jatindersingh382001@gmail.com>
2024-11-21 19:23:04 +0530

    header

--- frontend/crypto-exchange/src/components/Header.jsx
+++ frontend/crypto-exchange/src/components/Header.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css";
 
-const Header = () =>{
+
+const Header = () => {
     return (
         <header className="header">
-            <div className="logo">
-                <Link to ="/">My App</Link>
-            </div>
-            <nav className="navigation">
-                <ul>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/services">Services</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
+            <div className='navbar'>
+
+                <ul className="nbar">
+                    signin
                 </ul>
-            </nav>
+                <ul className="nbar">
+                    signup
+                </ul>
+
+
+
+            </div>
+
 
         </header>
 
